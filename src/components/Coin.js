7446947d1d2dcad451b9e83847213577a2d71da8
Polina <106979919+kollarbone@users.noreply.github.com>
2023-04-27 18:02:21 +0300

    Update Coin.js

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -1,10 +1,16 @@
+import { useState } from "react";
+
 export default function Product_card(props) {
   const data = props.data;
+  const [showDet, setShowDet] = useState(false);
   return (
     <>
       {data ? (
         <>
-          <div className="flex flex-row items-center justify-between border-t  p-5 hover:text-orange-300 hover:scale-105 border-top-white cursor-pointer">
+          <div
+            onClick={() => setShowDet(!showDet)}
+            className="flex flex-row items-center justify-between border-t  p-5 hover:text-orange-300 hover:scale-105 border-top-white cursor-pointer"
+          >
             <p className="text-xl text-orange-300">{data.symbol}</p>
             <p className="text-base">{data.name}</p>
             <p className="text-base">{data.price_usd} USD</p>
@@ -13,6 +19,14 @@ export default function Product_card(props) {
             </p>
             <p className="text-base">{data.market_cap_usd} USD</p>
           </div>
+          {showDet && (
+            <div className="flex flex-row items-center justify-between p-5">
+              <p className="text-base">Rank: {data.rank}</p>
+              <p className="text-base">1h Change: {data.percent_change_1h}</p>
+              <p className="text-base">Price BTC: {data.price_btc}</p>
+              <p className="text-base">7d Change: {data.percent_change_7d}</p>
+            </div>
+          )}
         </>
       ) : (
         <div>no data</div>
